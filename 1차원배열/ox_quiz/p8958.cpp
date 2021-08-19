3d20f5b23fa6bcb27d162f2e1f4b8d5c970e8517
thals1214 <thaxls@ewhain.net>
2021-08-19 23:29:14 +0900

    Update p8958.cpp
    strlen 쓸 때 -> include <string,h>해주기

--- 1차원배열/ox_quiz/p8958.cpp
+++ 1차원배열/ox_quiz/p8958.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#define NULL 0
 
 int main() {
-	int num_of_test;
-	int arr[80] = { NULL };
+	int num_of_test, score = 0, total_score = 0;
+	char arr[80];
 	std::cin >> num_of_test;
 	
 	for (int i = 0; i < num_of_test; i++) {
+		std::cin >> arr;
 
+		for (int j = 0; j < strlen(arr); j++) {
+			if (arr[j] == 'O') {
+				score++;
+				total_score += score;
+			}
+			else {
+				score = 0;
+			}
+		}
+		std::cout << total_score << "\n";
+		score = 0;
+		total_score = 0;
+		
 	}
 }
