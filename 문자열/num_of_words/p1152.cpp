e661ebe0f5f282fdbd134f384353316ff539171e
thals1214 <thaxls@ewhain.net>
2021-08-20 17:47:28 +0900

    Create p1152.cpp

---
+++ 문자열/num_of_words/p1152.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string sentence;
+	getline(cin, sentence);
+	int num = 0;
+
+	for (int i = 0; i < sentence.length(); i++) {
+		
+	}
+
+}
