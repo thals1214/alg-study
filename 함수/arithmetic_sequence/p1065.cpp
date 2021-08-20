9998fa4860f74a1b54cd4bd9ffd84d7788b3f209
thals1214 <thaxls@ewhain.net>
2021-08-20 12:59:06 +0900

    Create p1065.cpp

---
+++ 함수/arithmetic_sequence/p1065.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void sequence(int i) {
+	int sum = 0;
+	if (i < 100) {
+		std::cout << i << "\n";
+	}
+	else if (i <= 1000) {
+		sum += 99;
+		for (int j = 100; j <= i; j++) {
+			if (j / 100 - (j % 100) / 10 == (j % 100) / 10 - (j % 100) % 10)
+				sum++;
+		}
+		std::cout << sum << "\n";
+	}
+	
+}
+
+int main() {
+	int user_input;
+	std::cin >> user_input;
+	
+	sequence(user_input);
+}
