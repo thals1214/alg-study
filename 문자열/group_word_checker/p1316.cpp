938c2b5d70f5dba6678d87a907eeb586fe206b12
thals1214 <thaxls@ewhain.net>
2021-08-22 00:03:26 +0900

    Update p1316.cpp

--- 문자열/group_word_checker/p1316.cpp
+++ 문자열/group_word_checker/p1316.cpp
@@ -4,24 +4,22 @@
 using namespace std;
 int main() {
 	string str; int user_input;
-	char temp;
-	int cnt = 0, int idx;
-	bool flag = true;
 	cin >> user_input;
+	int cnt = 0;
 
 	for (int i = 0; i < user_input; i++) {
 		cin >> str;
-		temp = str[0];
-		for (int j = 1; j < str.length(); j++) {
-			if (j != temp) {
-				flag = false;
-				idx = str.find(temp);
-				if (idx == string::npos)
-					continue;
-				temp = str[j];
+		int arr[26] = { 0, };
+		int flag = 0;
+		for (int j = 0; j < str.length(); j++) {
+			arr[(int)str[j] - 97]++;
+			if (arr[(int)str[j] - 97] > 1 && str[j] != str[j - 1]) {
+				flag = 1;
+				break;
 			}
 		}
-		
+		if (flag == 0) cnt++;
 	}
+	cout << cnt;
 
 }
