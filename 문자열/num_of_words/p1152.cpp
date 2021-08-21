20c48e40baac2f962683bb9b2a6032782f81111d
thals1214 <thaxls@ewhain.net>
2021-08-21 13:32:25 +0900

    Update p1152.cpp

--- 문자열/num_of_words/p1152.cpp
+++ 문자열/num_of_words/p1152.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
 int main() {
 	string sentence;
 	getline(cin, sentence); // getline으로 sentence받기 
-	istringstream ss(sentence);
 
 	int cnt = 0;
+	bool space = false;
 
-
-	for (int i = 0; i < sentence.length(); i++) {
-		
+	if (sentence.empty()) {
+		cout << "0";
+		return 0;
 	}
+	cnt = 1;
+	for (int i = 0; i < sentence.length(); i++) {
+		if (sentence[i] == ' ')
+			cnt++;
+	}
+	if (sentence[0] == ' ')
+		cnt--;
+	if (sentence[sentence.length() - 1] == ' ')
+		cnt--;
+
+	cout << cnt;
+	return 0;
 
 }
