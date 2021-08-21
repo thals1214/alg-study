ee45fa2a2e8dc605721c36f07d07bb48bd586b16
thals1214 <thaxls@ewhain.net>
2021-08-21 12:38:31 +0900

    Update p1152.cpp

--- 문자열/num_of_words/p1152.cpp
+++ 문자열/num_of_words/p1152.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int main() {
 	string sentence;
-	getline(cin, sentence);
-	int num = 0;
+	getline(cin, sentence); // getline으로 sentence받기 
+	istringstream ss(sentence);
+
+	int cnt = 0;
+
 
 	for (int i = 0; i < sentence.length(); i++) {
 		
