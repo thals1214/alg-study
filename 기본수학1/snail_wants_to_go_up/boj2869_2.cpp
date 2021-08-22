7d5b4d54cbef87edc511a739c2c3c26d6afac1d6
thals1214 <thaxls@ewhain.net>
2021-08-22 13:29:00 +0900

    Update boj2869_2.cpp

--- 기본수학1/snail_wants_to_go_up/boj2869_2.cpp
+++ 기본수학1/snail_wants_to_go_up/boj2869_2.cpp
@@ -5,10 +5,15 @@ int main() {
 	int A, B, V;
 	cin >> A >> B >> V;
 
-	int height = 0;
-	int num = V / (A - B);
-
-	if (V % (A - B) == 0) {
-		height = (num - 1) * (A - B) + A;
+	int height = 0, cnt = 0;
+	while (height <= V) {
+		cnt++;
+		height += A - B;
+		
+		if (height + B >= V) {
+			cout << cnt;
+			return 0;
+		}
 	}
+	cout << cnt;
 }
