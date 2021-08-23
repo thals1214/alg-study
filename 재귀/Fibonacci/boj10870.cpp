8acdcb23e815ab47290a79d02c5ed167e74ad294
thals1214 <thaxls@ewhain.net>
2021-08-23 13:38:21 +0900

    Update boj10870.cpp

--- 재귀/Fibonacci/boj10870.cpp
+++ 재귀/Fibonacci/boj10870.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int fib(int n) {
 	if (n == 0) return 0;
-	else if (n == 1) return 1;
-	else {
-		return fib(n - 1) + fib(n - 2);
-	}
+	if (n == 1) return 1;
+	
+	return fib(n - 1) + fib(n - 2);
+	
 }
 int main() {
 	int n;
