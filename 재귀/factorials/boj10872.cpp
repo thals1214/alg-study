1ba301c340703e33db15915ee843dd81e11e8801
thals1214 <thaxls@ewhain.net>
2021-08-23 13:26:51 +0900

    Create boj10872.cpp

---
+++ 재귀/factorials/boj10872.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int N) {
+	if (N == 0) return 1;
+	if (N == 1) return 1;
+	return N * factorial(N - 1);
+}
+int main() {
+	int N;
+	cin >> N;
+	int num = factorial(N);
+
+	cout << num;
+}
