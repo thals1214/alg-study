35cec9ed79e43bf7b60e175913e3a02917a89430
thals1214 <thaxls@ewhain.net>
2021-08-23 15:11:35 +0900

    Update boj2447.cpp

--- 재귀/star_drawing/boj2447.cpp
+++ 재귀/star_drawing/boj2447.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int star_draw(int n) {
-	
+void star_draw(int i, int j, int n) {
+	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) cout << ' ';
+	else {
+		if (n / 3 == 0) cout << "*";
+		else star_draw(i, j, n / 3); 
+	}
 }
 
 int main() {
 	int n;
 	cin >> n;
-	star_draw(n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			star_draw(i, j, n);
+		}
+		cout << "\n";
+	}
 }
