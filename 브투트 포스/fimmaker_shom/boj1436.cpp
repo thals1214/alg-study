b62f29862ac4ca138506a757262453c29dc8886f
thals1214 <thaxls@ewhain.net>
2021-08-26 12:24:13 +0900

    Update boj1436.cpp

--- 브투트 포스/fimmaker_shom/boj1436.cpp
+++ 브투트 포스/fimmaker_shom/boj1436.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main() {
 	int N;
+	cin >> N;
 
+	int cnt = 0;
+	int start = 666, temp;
+	while (cnt != N) {
+		temp = start;
+		do {
+			if (temp % 1000 == 666) {
+				cnt++;
+				break;
+			}
+			temp /= 10;
+		} while (temp > 0);
+
+		if (cnt == N) {
+			cout << start;
+		}
+		start++;
+	}
 }
