4e077cf470163ee7a97232b9892fbf5716792cad
thals1214 <thaxls@ewhain.net>
2021-08-29 21:39:52 +0900

    왜 틀릴까 ;_;

---
+++ 정렬/sorting-coordinates-2/boj11651.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// driver function to sort the 2D matrix
+// on basis of a particular column;
+bool sort_second_col(const vector<int>& v1, const vector<int>& v2) {
+	return v1[1] < v2[1];
+}
+
+int main() {
+	int N;
+	cin >> N;
+
+	// vector array ¼±¾ð 
+	vector <vector<int>> arr(N, vector<int>(2, 0));
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < 2; j++) {
+			cin >> arr[i][j];
+		}
+	// sorting on basis of 2nd column
+	sort(arr.begin(), arr.end());
+	sort(arr.begin(), arr.end(), sort_second_col);
+	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 2; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
