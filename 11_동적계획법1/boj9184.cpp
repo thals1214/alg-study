007de36603767d9491d695fad24228f66b322681
thals1214 <thaxls@ewhain.net>
2021-09-05 23:40:27 +0900

    Create boj9184.cpp

---
+++ 11_동적계획법1/boj9184.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int w(int a, int b, int c) {
+	vector<int> arr;
+	arr.assign()
+
+
+	return val;
+}
+
+int main() {
+	int a, b, c;
+	int result;
+	while (true) {
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1) break;
+		result = w(a, b, c);
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << result << "\n";
+			
+	}
+}
