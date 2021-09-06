8f290eb93e82b2350d4ec3dce8f3ea9abaf64f9f
thals1214 <thaxls@ewhain.net>
2021-09-06 15:32:20 +0900

    Delete p10951.cpp

--- while문/pb10951-eos/p10951.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	int user_input1, user_input2;
-
-	while (std::cin >> user_input1 >> user_input2) {
-		std::cout << user_input1 + user_input2 << std::endl;
-	}
-}
