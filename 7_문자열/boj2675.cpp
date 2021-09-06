513c9e95e7cda70fd521c3f0abd9099848bd29a7
thals1214 <thaxls@ewhain.net>
2021-09-06 15:45:55 +0900

    Create boj2675.cpp

---
+++ 7_문자열/boj2675.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	int test_case, repeat_num;
+
+	cin >> test_case;
+
+	for (int i = 0; i < test_case; i++) {
+		cin >> repeat_num >> s;
+
+		for (int j = 0; j < s.length(); j++) {
+			for (int k = 0; k < repeat_num; k++) {
+				cout << s[j];
+			}
+		}
+		cout << "\n";
+	}
+}
