e9d478d35c36bafb546830c1ca49e62906f9660c
thals1214 <thaxls@ewhain.net>
2021-09-06 15:45:50 +0900

    Create boj2941.cpp

---
+++ 7_문자열/boj2941.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	int idx;
+	cin >> s;
+
+	string arr[8] = { "c=", "c-","dz=","d-","lj","nj","s=","z=" };
+
+	for (int i = 0; i < 8; i++) {
+		while (1) {
+			idx = s.find(arr[i]);
+			if (idx == string::npos)
+				break;
+			s.replace(idx, arr[i].length(), "#");
+		}
+	}
+	cout << s.length();
+}
