87ae92c69b6c81ae4f40bcdab57887ad4c73a1a0
thals1214 <thaxls@ewhain.net>
2021-09-13 23:24:06 +0900

    Update boj1259.cpp

--- epper/epper_17th/boj1259.cpp
+++ epper/epper_17th/boj1259.cpp
@@ -1,1 +1,24 @@
 /* low */
+#include <iostream>
+
+using namespace std;
+
+void calFelindrome(int n) {
+	int original_input = n , reversed_input = 0;
+	
+	while (n > 0) {
+		reversed_input = reversed_input * 10 + n % 10;
+		n /= 10;
+	}
+	if (reversed_input == original_input) cout << "yes\n";
+	else cout << "no\n";
+}
+
+int main() {
+	int n;
+	while (true) {
+		cin >> n;
+		if (n == 0) break;
+		calFelindrome(n);
+	}
+}
