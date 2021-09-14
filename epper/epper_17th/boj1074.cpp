8c01c950738f5b5e24d960a287a92a6f2d7f105c
thals1214 <thaxls@ewhain.net>
2021-09-14 16:53:41 +0900

    -ing

---
+++ epper/epper_17th/boj1074.cpp
@@ -0,0 +1,19 @@
+/* medium */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int cnt = 0;
+
+void z(int n, int r, int c, int row, int col) {
+	if (n == 1) {
+
+	}
+}
+int main() {
+	int n, r, c;
+	cin >> n >> r >> c;
+
+	z(n, r, c , 0, 0);
+	
+}
