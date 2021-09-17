c0fe944876d1b80a18381c5422e418e3bbe24b21
thals1214 <thaxls@ewhain.net>
2021-09-17 14:52:06 +0900

    Create pgmrs_43238.cpp

---
+++ epper/epper_17th/pgmrs_43238.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = LLONG_MAX; // 자료형의 최대값;
+
+    sort(times.begin(), times.end());
+    long long start = 1;
+    long long end = (long long)n * (long long)times[times.size() - 1];
+    long long mid, temp = 0;
+
+    while (start <= end) {
+        mid = (start + end) / 2;
+        temp = 0;
+        for (int i = 0; i < times.size(); i++) {
+            temp += mid / times[i];
+        }
+        if (temp >= n) {
+            answer = min(answer, mid);
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+    }
+    return answer;
+}
