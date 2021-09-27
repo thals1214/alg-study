b2c71b465ffe8c4bfa35415639f04d36b56de95b
thals1214 <thaxls@ewhain.net>
2021-09-27 12:32:43 +0900

    Create boj2231.cpp

---
+++ altu-bitu/day6-bruteforce/boj2231.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int constructor(int n) {
+	// n까지 분해합 모두 구해보기 
+	for (int i = 1; i < n; i++) {
+		int k = i; // i는 가공하면 안되므로 k에 저장
+		int sum = i;
+		while (k > 0) {
+			sum += (k % 10);
+			k /= 10;
+		}
+		if (sum == n) // 분해합이 n이라면 바로 생성자 (i) 리턴
+			return i;
+	}
+	return 0;
+}
+
+int main() {
+	int n; 
+	cin >> n;
+
+	cout << constructor(n) << '\n';
+}
